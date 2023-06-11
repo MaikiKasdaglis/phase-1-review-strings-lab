433f87b66a7ce4d3f895c02e8cfb48a34ae9fe76
Michael Kasdaglis <maikikasdaglis@gmail.com>
2023-06-10 20:39:50 -0600

    completes assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+let currentUser = "Penguin"
+let welcomeMessage = "Welcome to Flatbook, "; 
+welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
